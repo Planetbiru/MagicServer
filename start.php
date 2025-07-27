8855444c3558e8eb4f7fca3ceb5d139977ae483d
Kamshory, MT <kamshory@yahoo.com>
2025-07-27 18:30:00 +0700

    Update Redis Config

--- start.php
+++ start.php
@@ -36,7 +36,9 @@ ensureDirectory(__DIR__ . "/apache/logs");
 // Replace config templates
 replaceAndWrite(__DIR__ . "/config/httpd-template.conf", __DIR__ . "/config/httpd.conf");
 replaceAndWrite(__DIR__ . "/config/php-template.ini", __DIR__ . "/php/php.ini");
+replaceAndWrite(__DIR__ . "/config/my-template.ini", __DIR__ . "/mysql/my.ini");
 replaceAndWrite(__DIR__ . "/config/redis.windows-template.conf", __DIR__ . "/redis/redis.windows.conf");
+replaceAndWrite(__DIR__ . "/config/redis.windows-service-template.conf", __DIR__ . "/redis/redis.windows-service.conf");
 
 
 echo "=== MagicAppBuilder Portable Installer ===\n";
